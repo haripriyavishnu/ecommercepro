0ce9358cd8cee31a1f6bcf2a907761388edd441e
haripriyavishnu <150226362+haripriyavishnu@users.noreply.github.com>
2024-03-29 21:40:38 +0530

    Update settings.py

--- ecommerceproject/settings.py
+++ ecommerceproject/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'ecommerceproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'db.mysql',
     }
 }
 
